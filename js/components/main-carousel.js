b1768aee976b9ae2996f46c241ca0fc5bf6e5de1
Matheus Oliveira <matheusnagato300@gmail.com>
2021-05-06 23:30:37 -0300

    Create js/components folder

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,11 +1,11 @@
 const wrapperCarousel = document.querySelector('.wrapperCarousel');
 const scrollerCarousel = wrapperCarousel.querySelector('.scroller');
-const rightArrow = wrapperCarousel.querySelector('.main-arrow.-right');
-const leftArrow = wrapperCarousel.querySelector('.main-arrow.-left');
+const rightBtn = wrapperCarousel.querySelector('.main-arrow.-right');
+const leftBtn = wrapperCarousel.querySelector('.main-arrow.-left');
 const imgWidth = wrapperCarousel.querySelector('.wrapper').clientWidth;
   
-rightArrow.addEventListener('click', scrollToNextItem);
-leftArrow.addEventListener('click', scrollToPrevItem);
+rightBtn.addEventListener('click', scrollToNextItem);
+leftBtn.addEventListener('click', scrollToPrevItem);
   
 function scrollToNextItem() {
    scrollerCarousel.scrollBy({left: imgWidth, top: 0, behavior: 'smooth'});
